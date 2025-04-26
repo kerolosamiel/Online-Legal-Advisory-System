f4dcd7e2bc0691baae1bde3409de49947b63dddb
Mohamed Saad <149174851+m7medmsa3d@users.noreply.github.com>
2025-04-26 16:30:21 +0300

    add invoicerepository and servicerepository

---
+++ ELawyer.DataAccess/Repository/InvoiceRepository.cs
@@ -0,0 +1,33 @@
+﻿using BookStore.DataAccess.Repository;
+using ELawyer.DataAccess.Data;
+using ELawyer.DataAccess.Repository.IRepository;
+using ELawyer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELawyer.DataAccess.Repository
+{
+    public class InvoiceRepository : Repository<Invoice>, IInvoiceRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InvoiceRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void Update(Invoice obj)
+        {
+            Invoice invoice = _context.Invoices.FirstOrDefault(u => u.ID == obj.ID);
+            if (invoice != null)
+            {
+
+                invoice.CreatedAt = DateTime.Now;
+
+            }
+        }
+    }
+}
---
+++ ELawyer.DataAccess/Repository/ServiceOrderRepository.cs
@@ -0,0 +1,34 @@
+﻿using BookStore.DataAccess.Repository;
+using ELawyer.DataAccess.Data;
+using ELawyer.DataAccess.Repository.IRepository;
+using ELawyer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELawyer.DataAccess.Repository
+{
+    public class ServiceOrderRepository : Repository<ServiceOrders>, IServiceOrderRepository
+    {
+
+
+        private readonly ApplicationDbContext _context;
+
+        public ServiceOrderRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void Update(ServiceOrders obj)
+        {
+            ServiceOrders serviceorder = _context.ServiceOrders.FirstOrDefault(u => u.ID == obj.ID);
+            if (serviceorder != null)
+            {
+
+                serviceorder.ID = obj.ID;
+            }
+        }
+    }
+}
