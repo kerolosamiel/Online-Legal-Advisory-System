0efecc342ee4699a7ca3f1de3f25efbe5bcd1cc1
Mohamed Saad <149174851+m7medmsa3d@users.noreply.github.com>
2025-04-26 16:20:47 +0300

    add invoice and serviceorder tables

---
+++ ELawyer.Models/Invoice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELawyer.Models
+{
+    public class Invoice
+    {
+        public int ID { get; set; }
+        public int ServiceOrderID { get; set; }
+        public DateTime CreatedAt { get; set; }
+         public int paymentID { get; set; }
+        public double Amount { get; set; }
+
+        public int clientID { get; set; }
+       
+
+
+
+    }
+}
